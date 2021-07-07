a8b96a8be79e191495155095f8e04901e7b2f622
shehan <hasinthashehan768@gmail.com>
2021-07-07 14:35:06 +0530

    new router updates

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -20,12 +20,12 @@ function Facebookpage(props) {
     e.preventDefault();
   }
 
-  function postData(url = '', data = {}) {
+  async function postData(url = '', data = {}) {
 
-    return(new Promise((resolve,reject) => {
+    // return(new Promise((resolve,reject) => {
 
         // Default options are marked with *
-        fetch(url, {
+        const response = await fetch(url, {
           method: 'POST', // *GET, POST, PUT, DELETE, etc.
           mode: 'cors', // no-cors, *cors, same-origin
           cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -37,15 +37,13 @@ function Facebookpage(props) {
           redirect: 'follow', // manual, *follow, error
           referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
           body: JSON.stringify(data) // body data type must match "Content-Type" header
-        }).then(res => res.json())
-          .then(result => {resolve(result)});
-        // let res =  response.text();
+        });
+        return (response.json())
         
         // console.log(response);
         // return response; // parses JSON response into native JavaScript objects
 
-      })
-    );
+      };
 
   }
 
@@ -63,8 +61,9 @@ function Facebookpage(props) {
       // facebookAuthenticationCode = 'AQDlo_X6Zpj3ld7BAeVv4eS2wzsOMo7-mpRprCf1j8bjfdsirm1MgREdBW6Bam47I9FDNx5gw57Wpahy3XL0j1bbr7tbdfTxTTFaRtaR59nEDptPrW4Z1Vcg3pxL53heTg5XRsRnrTOcrAK-kPHdXr4Ip9mIqO58uSpbSzmV4lG0eL_A7SsxxZ3rw67wLGWjcHn9-qdzcFZPMZVSScglJm4gxYd3luXbX9oG6pID5k9A4l7fKe631u0X4UflW0Kg_BqdUJsoKWYGp2xQqyijdZ2U-tw1gcjL0fKc_j1L2Av2RQ6KVpb9_wEGqcjf3AgN8CHrn88MTP6RNADa0ei2oPYMtcNoNy-6nvhcyIUc3Qb5cQ#_=_';
       let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`;
       console.log(accessUrl);
-      const accessToken = postData(accessUrl);
-      console.log(accessToken);
+      postData(accessUrl)
+        .then(data => console.log(data))
+      // console.log(accessToken);
     }else{
       window.location = "https://www.facebook.com/v11.0/dialog/oauth?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F";
     }
