83d948a79f845b4226828f956b7f1541500f653d
shehan <hasinthashehan768@gmail.com>
2021-07-07 14:18:40 +0530

    new router updates

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -20,26 +20,33 @@ function Facebookpage(props) {
     e.preventDefault();
   }
 
-  let postData = (url = '', data = {}) => {
-    // Default options are marked with *
-    const response = fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        // 'Content-Type': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then(res => res.text())
-      .then(result => {return(result)});
-    // let res =  response.text();
-    
-    console.log(response);
-    return response; // parses JSON response into native JavaScript objects
+  function postData(url = '', data = {}) {
+
+    return(new Promise((resolve,reject) => {
+
+        // Default options are marked with *
+        const response = fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            // 'Content-Type': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data) // body data type must match "Content-Type" header
+        }).then(res => res.json())
+          .then(result => {resolve(result)});
+        // let res =  response.text();
+        
+        console.log(response);
+        return response; // parses JSON response into native JavaScript objects
+
+      })
+    );
+
   }
 
   const getQueryString = () => {
