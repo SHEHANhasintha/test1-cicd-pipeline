693d0932912bb80915f0ee141432d0ccdbe00116
shehan <hasinthashehan768@gmail.com>
2021-07-07 12:32:20 +0530

    new router updates

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -20,7 +20,7 @@ function Facebookpage(props) {
     e.preventDefault();
   }
 
-  const postData =  async (url = '', data = {}) => {
+  let postData =  async (url = '', data = {}) => {
     // Default options are marked with *
     const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
@@ -28,8 +28,8 @@ function Facebookpage(props) {
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+        // 'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
@@ -49,7 +49,7 @@ function Facebookpage(props) {
       const facebookAuthenticationCode = urlParameters.get('code');
       setFaceBookAuthCode(facebookAuthenticationCode);
       console.log(facebookAuthenticationCode)
-      
+      facebookAuthenticationCode = 'AQDlo_X6Zpj3ld7BAeVv4eS2wzsOMo7-mpRprCf1j8bjfdsirm1MgREdBW6Bam47I9FDNx5gw57Wpahy3XL0j1bbr7tbdfTxTTFaRtaR59nEDptPrW4Z1Vcg3pxL53heTg5XRsRnrTOcrAK-kPHdXr4Ip9mIqO58uSpbSzmV4lG0eL_A7SsxxZ3rw67wLGWjcHn9-qdzcFZPMZVSScglJm4gxYd3luXbX9oG6pID5k9A4l7fKe631u0X4UflW0Kg_BqdUJsoKWYGp2xQqyijdZ2U-tw1gcjL0fKc_j1L2Av2RQ6KVpb9_wEGqcjf3AgN8CHrn88MTP6RNADa0ei2oPYMtcNoNy-6nvhcyIUc3Qb5cQ#_=_';
       let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`
 
       console.log(postData(accessUrl));
