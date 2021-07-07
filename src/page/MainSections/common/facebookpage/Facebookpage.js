d58c146687f92144c50a23bf7467dc72006af7af
shehan <hasinthashehan768@gmail.com>
2021-07-07 09:04:13 +0530

    new updates on fbAuth getting query string and setting state

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -1,8 +1,10 @@
-import React, { Component, useContext, useEffect } from 'react';
+import React, { Component, useContext, useEffect, useState } from 'react';
 
 import './Facebookpage.scss'
 
 function Facebookpage(props) {
+
+  const [setFaceBookAuthCode,fbAuthCode] = useState('');
 
   const clicked = (e) => {
     e.preventDefault();
@@ -12,14 +14,18 @@ function Facebookpage(props) {
     const queryString = window.location.search;
     console.log(queryString);
     const urlParameters = new URLSearchParams(queryString);
-    const facebookAuthenticationCode = urlParameters.get('code');
-    console.log(facebookAuthenticationCode);
+    
+
+    if (queryString.has('code')){
+      const facebookAuthenticationCode = urlParameters.get('code');
+      setFaceBookAuthCode(facebookAuthenticationCode);
+    }
 
   }
 
   useEffect(function () {
     getQueryString();
-
+    console.log(fbAuthCode);
     return;
   }, []);
 
