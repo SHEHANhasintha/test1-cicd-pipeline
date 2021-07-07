f3412543575d6ff54cfbfb003ab6e7696ae9de6b
shehan <hasinthashehan768@gmail.com>
2021-07-07 13:17:27 +0530

    new router updates

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -34,9 +34,11 @@ function Facebookpage(props) {
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    console.log(response);
-    return response.json(); // parses JSON response into native JavaScript objects
+    })
+    let res =  response.text();
+    
+    console.log(res);
+    return res; // parses JSON response into native JavaScript objects
   }
 
   const getQueryString = () => {
