7bb16701d417f0afe586dbdae0581bf9f093e659
shehan <hasinthashehan768@gmail.com>
2021-07-07 11:57:32 +0530

    new router updates

--- README.md
+++ README.md
@@ -16,4 +16,23 @@ https://www.facebook.com/v11.0/dialog/oauth?
   client_id=232331721389865
   &redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F
 
-  
+
+
+
+
+https://graph.facebook.com/v11.0/oauth/access_token?
+  client_id=232331721389865
+  &redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F
+  &client_secret=88892166144044c04cc89cd33b0c5bd6
+  &code=<AUTHORIZATION_CODE>
+
+
+curl -i -X GET \
+ "https://graph.facebook.com/v11.0/1314251948683709/feed?access_token=EAADTTfG5NykBAILA8LeZB1vs54YaSfhYtTE3LBKzy2m6agVhl6DscpSmQ6vKZA8CH4MZCVPGFU2FYsPYfLPedkICAxsdVY2BN9WmPS0635M81vTWb1z86nZCmTZBpPhFeob202ZBhkD1R0gXx5vk28wZBMKM7hnMg7j6OHIF9bAZBXV1ayWPhMkadtpo84DL5ZC3ZCaDkBUJZAzwAZDZD&fields=child_attachments%2Cattachments%2Clikes.summary(true)"
+
+
+https://graph.facebook.com/v11.0/oauth/access_token?
+  client_id=232331721389865
+  &redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F
+  &client_secret=88892166144044c04cc89cd33b0c5bd6
+  &code=AQCymKm69IWLj7S0cLIG-kwnAMYmxgvP4zyxS4bCThlO4dW6SafGgTpwuqZNKjpNO8oHttVLUs8rRfvK3j3dxFM30QzAAnGjEJafQDnJv_3mRRfReAc-P-78oyNPAHR2IAdY-cEV3x6zlVW9Q3VxK55rjv0hUNz6bzKICwCr_8DDvSACuLAIxYCSC5U4nDY-YndMcShimDgxKEqau1eD7OaWIV5l6_ABqSTOwT5G2H8IZl0RSYZNvdWo0OCywIvgfKOBow9qsBCLASeA1MgT0mqTG16BaGwcv-Vp27zzXAsx4lBFnbdrDR9XfJe72969xeKlKH-Gu0Pf2aLnTY7iQHsSayN3Al6U-DJW1o0pxmVMsA#_=_
--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -20,6 +20,24 @@ function Facebookpage(props) {
     e.preventDefault();
   }
 
+  const postData =  async (url = '', data = {}) => {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
   const getQueryString = () => {
     
     const queryString = window.location.search;
@@ -30,6 +48,11 @@ function Facebookpage(props) {
     if (urlParameters.has('code')){
       const facebookAuthenticationCode = urlParameters.get('code');
       setFaceBookAuthCode(facebookAuthenticationCode);
+      console.log(facebookAuthenticationCode)
+      
+      let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`
+
+      console.log(postData(accessUrl));
     }else{
       window.location = "https://www.facebook.com/v11.0/dialog/oauth?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F";
     }
