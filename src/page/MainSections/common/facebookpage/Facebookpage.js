9662ecc3a96c113506d559bc411d60e63ecc2980
shehan <hasinthashehan768@gmail.com>
2021-07-08 18:03:09 +0530

    new updates on facebook authentication

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -10,29 +10,12 @@ import {
 } from "react-router-dom";
 
 
+
 import './Facebookpage.scss'
+import { postData } from './test';
 
 
 
-
-async function postData(url = '', body = undefined, method='GET') {
-  console.log("calling")
-  const response = await fetch(url, {
-    method, // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      // 'Content-Type': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body
-  })
-  return response;
-
-};
 
 
 
@@ -158,9 +141,10 @@ function Facebookpage(props) {
 
                       }else{
 
-                        setAccessToken(res.json().access_token)
+                        let reqData = res.json();  
+                        setAccessToken(reqData.access_token)
                         console.log(accessTokenClient);
-                        console.log(res.json().access_token);
+                        console.log(reqData.access_token);
                         return;
                       }
 
--- src/page/MainSections/common/facebookpage/test.js
+++ src/page/MainSections/common/facebookpage/test.js
@@ -1,3 +1,26 @@
-const facebookAuthenticationCode = '';
+// const facebookAuthenticationCode = '';
 
-let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`
+// let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`
+
+
+
+const postData = async (url = '', body = undefined, method='GET') => {
+    console.log("calling")
+    const response = await fetch(url, {
+      method, // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        // 'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body
+    })
+    return response;
+  
+  };
+
+  export { postData };
