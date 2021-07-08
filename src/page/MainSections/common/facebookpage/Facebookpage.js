d14efb91306c96db6a6a9ca7eaa3584072f180e5
shehan <hasinthashehan768@gmail.com>
2021-07-08 13:08:56 +0530

    new updates on facebook authentication

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -35,48 +35,7 @@ async function postData(url = '', body = undefined, method='GET') {
 
 };
 
-const getAccessKeyClient = async () => {
 
-  // setAccessToken("torpidooo");
-  
-
-
-  const queryString = window.location.search;
-  console.log(queryString);
-  const urlParameters = new URLSearchParams(queryString);
-
-
-  if (urlParameters.has('code')) {
-    const facebookAuthenticationCode = urlParameters.get('code');
-    let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`;
-
-
-
-    let data = await postData(accessUrl,undefined,'POST')
-                  // .then((res) => {
-                  //   setAccessToken(res.json().access_token)
-
-                  // })
-
-    if (data.status == 400) {
-      console.log("capability");
-      window.location = URI;
-    }
-
-    data = data.json();
-    const access_token = data.access_token;
-    // setAccessToken(data.access_token);
-    // console.log(accessTokenClient);
-    // return (access_token);
-    // console.log(accessTokenClient);
-
-    // return;
-
-  } else {
-    window.location = URI;
-  }
-
-}
 
 
 
@@ -89,6 +48,50 @@ function Facebookpage(props) {
     e.preventDefault();
   }
 
+
+  const getAccessKeyClient = async () => {
+
+    // return(setAccessToken("torpidooo"));
+    // setAccessToken("torpidooo");
+    
+    console.log("sss")
+  
+    // const queryString = window.location.search;
+    // console.log(queryString);
+    // const urlParameters = new URLSearchParams(queryString);
+  
+  
+    // if (urlParameters.has('code')) {
+    //   const facebookAuthenticationCode = urlParameters.get('code');
+    //   let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`;
+  
+  
+  
+    //   let data = await postData(accessUrl,undefined,'POST')
+    //                 // .then((res) => {
+    //                 //   setAccessToken(res.json().access_token)
+  
+    //                 // })
+  
+    //   if (data.status == 400) {
+    //     console.log("capability");
+    //     window.location = URI;
+    //   }
+  
+    //   data = data.json();
+    //   const access_token = data.access_token;
+      // setAccessToken(data.access_token);
+      // console.log(accessTokenClient);
+      // return (access_token);
+      // console.log(accessTokenClient);
+  
+      // return;
+  
+    // } else {
+    //   window.location = URI;
+    // }
+  
+  }
 
 
 
@@ -115,12 +118,54 @@ function Facebookpage(props) {
 
   // }
 
-  useEffect(getAccessKeyClient(), [accessTokenClient]);
+  useEffect(() => {
+
+
+    // return(setAccessToken("torpidooo"));
+    // setAccessToken("torpidooo");
+    
+    console.log("sss")
+  
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParameters = new URLSearchParams(queryString);
+  
+  
+    if (urlParameters.has('code')) {
+      const facebookAuthenticationCode = urlParameters.get('code');
+      let accessUrl = `https://graph.facebook.com/v11.0/oauth/access_token?client_id=232331721389865&redirect_uri=https%3A%2F%2Fmaster.d2fkzzti19cg91.amplifyapp.com%2F&client_secret=88892166144044c04cc89cd33b0c5bd6&code=${facebookAuthenticationCode}`;
+  
+  
+  
+      let data = postData(accessUrl,undefined,'POST')
+                    // .then((res) => {
+                    //   setAccessToken(res.json().access_token)
+  
+                    // })
+  
+      if (data.status == 400) {
+        console.log("capability");
+        window.location = URI;
+      }
+  
+      data = data.json();
+      const access_token = data.access_token;
+      setAccessToken(access_token);
+      console.log(accessTokenClient);
+  
+    } else {
+      window.location = URI;
+    }
+  
+
+  }, [accessTokenClient]);
+
+
 
   return (
     <>
 
-      {}
+      <p>{accessTokenClient}</p>
 
 
     </>
