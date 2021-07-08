41a60042feb86b3ff1fd1ef1be87fa9220f7f730
shehan <hasinthashehan768@gmail.com>
2021-07-08 10:47:40 +0530

    new updates on facebook authentication

--- src/page/MainSections/common/facebookpage/Facebookpage.js
+++ src/page/MainSections/common/facebookpage/Facebookpage.js
@@ -12,6 +12,28 @@ import {
 
 import './Facebookpage.scss'
 
+
+async function postData(url = '', body = undefined, method='GET') {
+
+  const response = await fetch(url, {
+    method, // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      // 'Content-Type': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body
+  })
+  return response;
+
+};
+
+
+
 function Facebookpage(props) {
   const [accessTokenClient, setAccessToken] = useState(false);
   const [accessTokenPage, setAccessTokenPage] = useState(false);
@@ -51,24 +73,7 @@ function Facebookpage(props) {
   useEffect(function () {
 
 
-    async function postData(url = '', body = undefined, method='GET') {
 
-      const response = await fetch(url, {
-        method, // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          // 'Content-Type': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body
-      })
-      return response;
-  
-    };
 
 
     const getAccessKeyClient = async () => {
