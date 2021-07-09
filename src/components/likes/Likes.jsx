f5a3cb986347b4411ce60052d72bc68e456e17c7
shehan <hasinthashehan768@gmail.com>
2021-07-09 11:31:49 +0530

    new updates on facebook authentication

--- src/components/likes/Likes.jsx
+++ src/components/likes/Likes.jsx
@@ -2,20 +2,25 @@ import React, {useEffect,useState} from "react";
 import "./Likes.scss";
 import { getData } from "../../helpers/helper";
 
-const getPageToken = (accessToken) => {
-  console.log("llllllllllllllllllllllllll")
-  console.log(2, accessToken)
-  if (accessToken) {
-    let accessUrl = `https://graph.facebook.com/v11.0/1314251948683709?fields=access_token&access_token=${accessToken}`
-    let data = getData(accessUrl)
-    console.log(data)
-    return data;
-  }
 
-}
 
 const Likes = (props) => {
   let [pageToken,setPageToken] = useState(false);
+
+  const getPageToken = (accessToken) => {
+    console.log("llllllllllllllllllllllllll")
+    console.log(2, accessToken)
+    if (accessToken) {
+      let accessUrl = `https://graph.facebook.com/v11.0/1314251948683709?fields=access_token&access_token=${accessToken}`
+      getData(accessUrl)
+        .then((data) => {
+          console.log(data);
+          setPageToken(data.access_token)
+        })
+      return;
+    }
+  
+  }
 
   getPageToken(props.accessToken);
 
@@ -24,12 +29,9 @@ const Likes = (props) => {
       {
         () => {
           getPageToken(props.accessToken)
-            .then((data) => {
-              setPageToken(data.access_token)
-              return <span>pageToken: {pageToken}</span>;
-            })
         }
       }
+      <p>pageToken: {pageToken}</p>;
     </div>
   );
 };
