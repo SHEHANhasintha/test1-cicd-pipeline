996e88d717906c1534db64e5abbd982090f0888d
shehan <hasinthashehan768@gmail.com>
2021-07-09 16:03:19 +0530

    new updates on facebook authentication

--- src/components/likes/Likes.jsx
+++ src/components/likes/Likes.jsx
@@ -2,22 +2,28 @@ import React, { useEffect, useState } from "react";
 import "./Likes.scss";
 import { getData } from "../../helpers/helper";
 
+const extractData = async (props,setMessage) => {
 
+  if (!props.accessToken){
+    return
+  }
+  console.log("caveeeeee",props.accessToken)
+  const accessUrl = `https://graph.facebook.com/v11.0/1314251948683709_1406401799468723?access_token=${props.accessToken}`
+  let data = await getData(accessUrl)
+
+  console.log(data.message);
+  setMessage(data.message);
+}
 
 const Likes = (props) => {
   const [message,setMessage] = useState(false);
 
-  useEffect(() => {
-    if (!props.accessToken){
-      return
-    }
-    console.log("caveeeeee",props.accessToken)
-    const accessUrl = `https://graph.facebook.com/v11.0/1314251948683709_1406401799468723?access_token=${props.accessToken}`
-    let data = getData(accessUrl)
 
-    console.log(data.message);
-    setMessage(data.message);
-  }, [props.accessToken,message])
+
+
+  useEffect(() => {
+    extractData(props,setMessage);
+  }, [props, props.accessToken])
 
 
   return (
