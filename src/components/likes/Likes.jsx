32a8ed41659d09158a392e4ec0577546b2b76e4d
shehan <hasinthashehan768@gmail.com>
2021-07-09 15:54:27 +0530

    new updates on facebook authentication

--- src/components/likes/Likes.jsx
+++ src/components/likes/Likes.jsx
@@ -1,29 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./Likes.scss";
-import { getData,getDatatemp } from "../../helpers/helper";
+import { getData } from "../../helpers/helper";
 
 
 
 const Likes = (props) => {
   const [message,setMessage] = useState(false);
 
-  const extractData = async () => {
-
-
+  useEffect(() => {
+    if (!props.accessToken){
+      return
+    }
     console.log("caveeeeee",props.accessToken)
     const accessUrl = `https://graph.facebook.com/v11.0/1314251948683709_1406401799468723?access_token=${props.accessToken}`
-    let data = await getDatatemp(accessUrl)
-
-    // console.log(data.json().message);
+    let data = getData(accessUrl)
 
     console.log(data.message);
     setMessage(data.message);
-    //return <p>{message}</p>
-  }
-
-
-  useEffect(() => {
-    extractData();
   }, [props.accessToken])
 
 
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -17,36 +17,11 @@ const reqData = async (url = '', body = undefined, method = 'GET') => {
 
 };
 
-const reqDatatemp = async (url = '', body = undefined, method = 'GET') => {
-    console.log("calling")
-    const response = await fetch(url, {
-        method, // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body
-    })
-    console.log(response)
-    return response.json();
-
-};
-
 const postData = async (accessUrl) => {
     const resData = await reqData(accessUrl, undefined, 'POST')
     return resData;
 }
 
-const getDatatemp = async (accessUrl) => {
-    const resData = await reqDatatemp(accessUrl, undefined, 'GET')
-    console.log("rrrrrrrrrrrrrrrr")
-    return resData;
-}
 
 const getData = async (accessUrl) => {
     const resData = await reqData(accessUrl, undefined, 'GET')
@@ -54,4 +29,4 @@ const getData = async (accessUrl) => {
     return resData;
 }
 
-export { reqData, postData, getData,getDatatemp };
+export { reqData, postData, getData };
